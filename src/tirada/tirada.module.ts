03f1d0d703774f7391dc098705d0d76e23228a77
Sergio_Arias <148729515+SaiPlusSai@users.noreply.github.com>
2025-07-29 20:57:21 -0400

    vf
    vf

--- src/tirada/tirada.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TiradaController } from './tirada.controller';
-import { TiradaService } from './tirada.service';
-
-@Module({
-  controllers: [TiradaController],
-  providers: [TiradaService]
-})
-export class TiradaModule {}
